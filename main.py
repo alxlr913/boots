a0e767ab949817e889345267c731171f5c10b901
Ibarra <llibarra@cps.k12.il.us>
2025-04-04 11:59:02 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+print("I like pizza!")
+print("It's really good!")
+# Variable +container for a value (string, integer, float, boolean)
+#                A variable behaves as if it was the vaalue it contains
+first_name= "Bro"
+# print(first_name)
+
+food = "pizza"
+print(food)
+# f-string allows you to insert a variable
+# into a string
+print(f"Hello {first_name}")
+print(f"You like {food}")
+
+first_name = "Bro"
+food = "pizza"
---
+++ main2.py
@@ -0,0 +1,36 @@
+# what are variables?
+
+# there are different types of variables
+# strings are text
+# name= "John" #quotation marks
+#name2= "Lucy"
+# integers are whole numbers
+#num1="10" # no quotation marks
+#num2=20
+#print(int(num1)+num2)
+# when you have a plus sign betrween two variables its called concatenatiom
+# floats are decimal numbers
+# typecasting is when you convert a variable from one type to another
+
+# f strings allows us to insert variables into strings
+# using f before the string
+print(f"{name}and {name2} have {num1} apples")
+# it can be positivce or negative
+dollars= 10.99
+print (f"{name} has {dollars} dollars")
+# booleans arew true or false
+is_student = True
+# It can be true or false
+print(f" {name} is a student: {is_ student}")
+# lists are collections of values
+# dictionaries are collections of key-values pairs
+# problem set #1
+# 1. create 5 diff types of variables
+# 2. print them out
+# 3. use f strings to format the strings
+
+name= "hunty"
+name2= "boots"
+food = "strawberries"
+num1= 420
+print(f"{name} and {name2} bought {num1} strawberries to make jam")
